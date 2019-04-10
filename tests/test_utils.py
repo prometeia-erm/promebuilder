5886ab1e84ccb6ca79a6a408aa706fbda603c57c
Denis Brandolini <denis.brandolini@prometeia.com>
2019-04-10 15:13:08 +0200

    Cambio formato versioni prodotte e label diversa hotfix e release

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+import pytest
 from promebuilder.utils import gen_metadata, gen_ver_build
 
 
@@ -15,15 +16,22 @@ def test_gen_ver_build():
     assert ('1.2.3.zuppone', 99, '') == gen_ver_build('1.2.3.zuppone', 'master', 99)
     # No branch: dev0
     assert '1.2.3a0' == gen_ver_build('1.2.3', '', 99)[0]
-    # Branch master
+    # Branch master, support
     assert ('1.2.3', 0, 'main') == gen_ver_build('1.2.3', 'master', 99)
+    assert ('1.2.3', 0, 'main') == gen_ver_build('1.2.3', 'support/XXX', 99)
     # Branch develop
-    assert ('1.2.3a3', 99, 'develop') == gen_ver_build('1.2.3', 'develop', 99)
-    # Branch develop_XXX
-    assert ('1.2.3a2', 99, 'develop_refactor') == gen_ver_build('1.2.3', 'develop_refactor', 99)
+    assert ('1.2.3a4', 99, 'develop') == gen_ver_build('1.2.3', 'develop', 99)
+    # Branch develop/XXX
+    assert ('1.2.3a2', 99, 'refactor') == gen_ver_build('1.2.3', 'develop/refactor', 99)
     # Branch feature/XXX
-    assert ('1.2.3a1.dev1', 99, '') == gen_ver_build('1.2.3', 'feature/h725', 99)
+    assert ('1.2.3a3', 99, '') == gen_ver_build('1.2.3', 'feature/h725', 99)
     # Branch release/XXX
-    assert ('1.2.3rc1.dev99', 0, 'release') == gen_ver_build('1.2.3', 'release/ciccio', 99)
+    assert ('1.2.3rc99', 0, 'release') == gen_ver_build('1.2.3', 'release/ciccio', 99)
     # Branch hotfix/XXX
-    assert ('1.2.3rc2.dev99', 0, 'release') == gen_ver_build('1.2.3', 'hotfix/pluto', 99)
+    assert ('1.2.3rc99', 0, 'hotfix') == gen_ver_build('1.2.3', 'hotfix/pluto', 99)
+    # Invalid subname
+    with pytest.raises(AssertionError):
+        gen_ver_build('1.2.3', 'hotfix/develop', 99)
+    with pytest.raises(AssertionError):
+        gen_ver_build('1.2.3', 'develop/master', 99)
+
